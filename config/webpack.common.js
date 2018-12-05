19a423e5de21e048b9839dc84556eefe5ff87866
tangqi <tangqi@bilibili.com>
2018-12-05 14:54:56 +0800

    [update] change port

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,44 +1,44 @@
-const path = require('path') 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // const resolve = dir => path.join(__dirname, '../', dir)
 
 module.exports = {
-  entry: {
-    index: path.resolve(__dirname, '../src', 'index.js'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'development',
-      inject: 'body',
-      compile: true,
-      favicon: false,
-      minify:{ 
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true,
-        minifyJS: true, 
-      },
-      cache: true,
-      showErrors: true,
-      chunks: 'all',
-      excludeChunks: [],
-      chunksSortMode: 'auto',
-      template: path.resolve(__dirname, '../src', 'index.html'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
+    entry: {
+        index: path.resolve(__dirname, '../src', 'index.js'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'development',
+            inject: 'body',
+            compile: true,
+            favicon: false,
+            minify:{ 
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true,
+                minifyJS: true, 
+            },
+            cache: true,
+            showErrors: true,
+            chunks: 'all',
+            excludeChunks: [],
+            chunksSortMode: 'auto',
+            template: path.resolve(__dirname, '../src', 'index.html'),
+        }),
     ],
-  },
-}
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+};
